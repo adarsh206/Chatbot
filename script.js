62695077688952eb1e2ed138b041f74074375513
Adarsh Kumar <adarsh2061998@gmail.com>
2023-12-31 14:05:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
 
 let userMessage = null; // Variable to store user's message
-const API_KEY = "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"; // Paste your API key here
+const API_KEY = ""; // Paste your API key here
 const inputInitHeight = chatInput.scrollHeight;
 
 const createChatLi = (message, className) => {
